b802147e0f039775636da073012a7a6cc0d2c713
Derek Carter <goozbach@friocorte.com>
2011-04-07 12:15:07 -0400

    2011 refresh

--- pants.server.c
+++ pants.server.c
@@ -1,10 +1,23 @@
-// test.c  -- Goozbach's echo server.
-//            Cobbled together from crap found on the web.
-//            Guaranteed to be broken and insecure.  Please don't run with /etc/passwd as the file.  :)
+// server.c -- Goozbach's echo server.
+//             Cobbled together from crap found on the web.
+//             Guaranteed to be broken and insecure.  Please don't run with /etc/passwd as the file.  :)
 //
-//            Ryan Erickson
-//            ccoder@ericksonfamily.com
-//            http://www.ericksonfamily.com
+//             Ryan Erickson
+//             ccoder@ericksonfamily.com
+//             http://www.ericksonfamily.com
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 2 as published by the Free Software Foundation.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include <stdio.h>
 #include <sys/types.h>
